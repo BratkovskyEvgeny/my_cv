b44dff92dec145977861e7c49a9fe6f32cb6659c
Evgeny <120304735+BratkovskyEvgeny@users.noreply.github.com>
2023-05-26 22:20:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ st.markdown("""
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link enabled" href="/">Home <span class="sr-only">(current)</span></a>
+      <li class="nav-item">
+        <a class="nav-link href="/">Home<span</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#education">Education</a>
