1f550bd0300c9076b5128821f9ae5c04bb343171
Evgeny <120304735+BratkovskyEvgeny@users.noreply.github.com>
2023-05-27 00:31:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
-image = Image.open('photo-round.png')
+#image = Image.open('photo-round.png')
 
 
 
