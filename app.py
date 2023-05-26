78dee4aae2b73da8a71247cbbb26a4b4ea95cfa2
Evgeny <120304735+BratkovskyEvgeny@users.noreply.github.com>
2023-05-26 21:56:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -147,10 +147,10 @@ contact_form = """
 
 st.markdown(contact_form, unsafe_allow_html=True)
 
-#def local_css(file_name):
-    #with open(file_name) as f:
-        #st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-#local_css("style/style.css")
+local_css("style/style.css")
 
